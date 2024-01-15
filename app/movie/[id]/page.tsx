c823007ddbeca03319ffed4f0bf6562ca93eb316
Rishav Kalra <75754555+rishav1710@users.noreply.github.com>
2024-01-15 23:10:07 +0530

    Update page.tsx

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 async function Movie({ params }: any) {
   const movieId = params.id;
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
   );
   const movie = await res.json();
 
