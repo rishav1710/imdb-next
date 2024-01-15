f55be392ddbab440a2f769b509381a0706225643
Rishav Kalra <75754555+rishav1710@users.noreply.github.com>
2024-01-15 23:09:43 +0530

    Update page.tsx

--- app/search/[searchTerm]/page.tsx
+++ app/search/[searchTerm]/page.tsx
@@ -1,18 +1,50 @@
+'use client';
+import Loading from '@/app/loading';
+import { API_KEY } from '@/app/page';
 import HomePage from '@/src/components/HomePage';
+import { useEffect, useState } from 'react';
 
-export default async function SearchPage({ params }: any) {
-  const seachTerm = params.searchTerm;
-  const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
-  );
-  const data = await res.json();
-  const results = data.results;
+export default function SearchPage({ params }: any) {
+  const [page, setPage] = useState<number>(1);
+  const [data, setData] = useState<any>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const seachTerm = params.searchTerm;
+      const res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
+      );
+      if (!res.ok) {
+        throw new Error('DATA NOT FOUND');
+      }
+      const data = await res.json();
+      setData(data);
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  const handlePage = (page: number) => {
+    setPage(page);
+  };
+
+  if (!data) {
+    return <Loading />;
+  }
+
   return (
     <div>
-      {results && results.length === 0 && (
+      {data.results && data.results.length === 0 && (
         <h1 className="text-center pt-6">No results found</h1>
       )}
-      {results && <HomePage results={results} />}
+      {data.results && (
+        <HomePage
+          results={data.results}
+          totalPages={data.total_pages}
+          handlePage={handlePage}
+          currentPage={page}
+        />
+      )}
     </div>
   );
 }
