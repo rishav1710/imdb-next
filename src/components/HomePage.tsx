3995aa6e7da263a1b4ffdfb41f374f89056ac12d
Rishav Kalra <75754555+rishav1710@users.noreply.github.com>
2024-01-15 23:11:25 +0530

    Update HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,13 +1,41 @@
+'use client';
 import React from 'react';
 import Card from './Card';
+import { Pagination } from '@nextui-org/react';
 
-function HomePage({ results }: { results: any }) {
+function HomePage({
+  results,
+  totalPages,
+  handlePage,
+  currentPage,
+}: {
+  results: any;
+  totalPages: number;
+  currentPage: number;
+  handlePage: (page: number) => void;
+}) {
   return (
-    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
-      {results.map((result: any, index: number) => {
-        return <Card key={index} result={result} />;
-      })}
-    </div>
+    <>
+      <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
+        {results.map((result: any, index: number) => {
+          return <Card key={index} result={result} />;
+        })}
+      </div>
+      {results.length > 0 && (
+        <div>
+          <Pagination
+            color="warning"
+            isCompact
+            showControls
+            initialPage={1}
+            page={currentPage}
+            className="flex p-4 lg:text-lg justify-center gap-6"
+            total={totalPages}
+            onChange={(e) => handlePage(e)}
+          />
+        </div>
+      )}
+    </>
   );
 }
 
