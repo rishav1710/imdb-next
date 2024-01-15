1b136a14686b3cb564791a3b73047a209ea1e03c
Rishav Kalra <75754555+rishav1710@users.noreply.github.com>
2024-01-15 23:38:22 +0530

    Update page.tsx

--- app/search/[searchTerm]/page.tsx
+++ app/search/[searchTerm]/page.tsx
@@ -11,7 +11,7 @@ export default function SearchPage({ params }: any) {
     const fetchData = async () => {
       const seachTerm = params.searchTerm;
       const res = await fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
+        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&query=${seachTerm}&language=en-US&page=${page}&include_adult=false`
       );
       if (!res.ok) {
         throw new Error('DATA NOT FOUND');
