dce03d9210145aeb98e39bcc0645305e97d1a954
Rishav Kalra <75754555+rishav1710@users.noreply.github.com>
2024-01-15 23:12:05 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -16,7 +18,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
   darkMode:'class'
 }
 export default config
